b9e50f9a32446576e7a7fe931e2b64838a5d84d3
alexe <alexeydavidov.2001@gmail.com>
2022-11-15 11:17:12 +0300

    Start of replacement version comparison.

--- pkg/API/utils.go
+++ pkg/API/utils.go
@@ -2,7 +2,10 @@ package API
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func InSet(set map[string]Package, packName string) bool {
@@ -39,9 +42,14 @@ func Compare(first, second map[string]Package, path string) error {
 		if !InSet(second, name) {
 			difference.Add(1, pack)
 		} else {
-			if pack.Version > second[name].Version {
+			result := compareVersions(pack.Version, second[name].Version)
+			if result == -1 {
+				return errors.New("error compare versions")
+			}
+			if result == 1 {
 				difference.Add(3, pack)
 			}
+
 			delete(second, name)
 		}
 		delete(first, name)
@@ -53,4 +61,41 @@ func Compare(first, second map[string]Package, path string) error {
 	println("Unique elements in 2:", len(difference.SecondUniqueArray))
 	println("Elements with a difference version: ", len(difference.VersionDifference))
 	return writeJSON(path, difference)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func compareVersions(v1, v2 string) int {
+	splitedV1 := strings.Split(v1, ".")
+	splitedV2 := strings.Split(v2, ".")
+	l := min(len(splitedV1), len(splitedV2))
+	for i := 0; i < l; i++ {
+		n1, err := strconv.Atoi(splitedV1[i])
+		if err != nil {
+			println(v1)
+			errorLoggerAPI.Printf("compareVersions: %s", err.Error())
+			return -1
+		}
+		n2, err := strconv.Atoi(splitedV2[i])
+		if err != nil {
+			println(v2)
+			errorLoggerAPI.Printf("compareVersions: %s", err.Error())
+			return -1
+		}
+		if n1 > n2 {
+			return 1
+		} else if n1 < n2 {
+			return 0
+		}
+	}
+	if len(splitedV1) > len(splitedV2) {
+		return 1
+	}
+	return 0
 }
